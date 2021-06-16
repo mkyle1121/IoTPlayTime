1be73acdeb12166b99824cee22f76648c1b9e1ca
Mike kyle <mkyle1121@hotmail.com>
2021-06-15 20:55:30 -0500

    added keymatrix

--- KeypadMatrix/KeypadMatrix/Program.cs
+++ KeypadMatrix/KeypadMatrix/Program.cs
@@ -8,30 +8,57 @@ namespace KeypadMatrix
 {
     class Program
     {
+
+        //https://toscode.gitee.com/kinglinglive/iot/blob/dependabot/nuget/src/System.Device.Gpio.Tests/Moq-4.16.1/src/devices/KeyMatrix/README.md
         static void Main()
         {
-            IEnumerable<int> outputs = new int[] { 26, 19, 13, 6 };
-            IEnumerable<int> inputs = new int[] { 21, 20, 16, 12 };        
+            IEnumerable<int> outputs = new int[] { 6, 13, 19, 26 };
+            IEnumerable<int> inputs = new int[] { 12, 16, 20, 21 };        
             KeyMatrix keyMatrix = new KeyMatrix(outputs, inputs, TimeSpan.FromMilliseconds(20));
 
-            Console.WriteLine("Ready to Listen...");
-            keyMatrix.KeyEvent += KeyMatrix_KeyEvent;
-            keyMatrix.StartListeningKeyEvent();            
+            Console.WriteLine("Press a Button...");
+            //keyMatrix.KeyEvent += KeyMatrix_KeyEvent;
+            //keyMatrix.StartListeningKeyEvent();            
                         
-            while (!Console.KeyAvailable)
+            while (true)
             {
-                Thread.Sleep(1);         
-            }           
-        }
+                KeyMatrixEvent key = keyMatrix.ReadKey();
+                
+                if (key.Input == 0 && key.Output == 0 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("1");
+                else if (key.Input == 1 && key.Output == 0 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("2");
+                else if (key.Input == 2 && key.Output == 0 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("3");
+                else if (key.Input == 3 && key.Output == 0 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("A");
+                else if (key.Input == 0 && key.Output == 1 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("4");
+                else if (key.Input == 1 && key.Output == 1 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("5");
+                else if (key.Input == 2 && key.Output == 1 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("6");
+                else if (key.Input == 3 && key.Output == 1 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("B");
+                else if (key.Input == 0 && key.Output == 2 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("7");
+                else if (key.Input == 1 && key.Output == 2 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("8");
+                else if (key.Input == 2 && key.Output == 2 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("9");
+                else if (key.Input == 3 && key.Output == 2 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("C");
+                else if (key.Input == 0 && key.Output == 3 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("*");
+                else if (key.Input == 1 && key.Output == 3 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("0");
+                else if (key.Input == 2 && key.Output == 3 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("#");
+                else if (key.Input == 3 && key.Output == 3 && key.EventType == PinEventTypes.Rising)
+                    Console.WriteLine("D");
 
-        private static void KeyMatrix_KeyEvent(object sender, KeyMatrixEvent keyMatrixEvent)
-        {
-            var pin21 = new GpioController();
-            pin21.OpenPin(21);
-            if (pin21.Read(21) == PinValue.High)
-            {
-                Console.WriteLine("A was pressed?");
-            }
-        }
+                Thread.Sleep(1);         
+            }            
+        }        
     }
 }
