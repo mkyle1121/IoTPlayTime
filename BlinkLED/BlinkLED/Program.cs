1a5795361c743bef5a485416a4089ea5bac6e1ef
Mike kyle <mkyle1121@hotmail.com>
2021-07-24 20:10:58 -0500

    blinkLED

---
+++ BlinkLED/BlinkLED/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+namespace BlinkLED
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var controller = new GpioController();
+            controller.OpenPin(17, PinMode.Output);
+
+            while (true)
+            {
+                controller.Write(17, PinValue.High);
+                Thread.Sleep(1000);
+                controller.Write(17, PinValue.Low);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
