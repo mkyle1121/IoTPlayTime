74c6db226d0d0831f09647bd567a94ae948c7562
Mike kyle <mkyle1121@hotmail.com>
2021-07-24 22:26:50 -0500

    PWM

---
+++ PWM_LED/PWM_LED/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Device.Pwm;
+using System.Threading;
+
+
+namespace PWM_LED
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var pwm = PwmChannel.Create(0,0,400,.1);
+            pwm.Start();
+
+            while(true)
+            {
+                for (double i = 0; i <= 1; i += .1)
+                {
+                    pwm.DutyCycle = i;
+                    Thread.Sleep(50);
+                }
+
+                for (double i = 1; i > 0; i -= .1)
+                {
+                    pwm.DutyCycle = i;
+                    Thread.Sleep(50);
+                }
+            }
+
+           
+
+        }
+    }
+}
