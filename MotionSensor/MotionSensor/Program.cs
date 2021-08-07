4a2c41c18dd02a8bb2e329275cfdd7715e411ab6
Mike kyle <mkyle1121@hotmail.com>
2021-08-07 10:48:15 -0500

    motionsensor

---
+++ MotionSensor/MotionSensor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+using System.Diagnostics;
+
+namespace MotionSensor
+{
+    class Program
+    {
+        static void Main()
+        {       
+            var controller = new GpioController();
+            controller.OpenPin(17, PinMode.Input);         
+
+            while (true)
+            {
+                if (controller.Read(17) == PinValue.High)
+                {
+                    Console.WriteLine("Something is there!");
+
+                    DateTime date = DateTime.Now;
+                    TimeSpan time = date.TimeOfDay;
+
+                    Console.WriteLine("Taking A Picture!");
+                    ProcessStartInfo startInfo = new ProcessStartInfo("raspistill", $"-o /home/pi/images/{time}.jpg");
+                    Process process = Process.Start(startInfo);
+                    process.WaitForExit();
+                    Console.WriteLine("Done...");                                       
+
+                }
+                else if (controller.Read(17) == PinValue.Low)
+                {
+                    //Console.WriteLine("Nothing is there.");
+                    Thread.Sleep(50);
+                }
+            }
+        }
+    }
+}
