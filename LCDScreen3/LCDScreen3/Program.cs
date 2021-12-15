d256b3157c8cfccab7f694f7a29e1a34e76011bb
Mike kyle <mkyle1121@hotmail.com>
2021-12-14 18:39:58 -0600

    LCDScreen update

---
+++ LCDScreen3/LCDScreen3/Program.cs
@@ -0,0 +1,32 @@
+﻿using Iot.Device.Pcx857x;
+using System.Device.Gpio;
+using System.Device.I2c;
+using Iot.Device.CharacterLcd;
+
+namespace LCDScreen3
+{
+    class Program
+    {
+        static void Main()
+        {
+            using I2cDevice i2cDevice = I2cDevice.Create(new I2cConnectionSettings(1, 0x27));
+            using Pcf8574 serialDriver = new Pcf8574(i2cDevice);
+
+            using var lcd = new Lcd1602
+            (
+                dataPins: new int[] { 4, 5, 6, 7 },
+                registerSelectPin: 0,
+                readWritePin: 1,
+                enablePin: 2,
+                backlightPin: 3,
+                controller: new GpioController(PinNumberingScheme.Logical, serialDriver)
+            );
+
+            lcd.Clear();
+            lcd.SetCursorPosition(2, 0);
+            lcd.Write("That Wassup!");
+            lcd.SetCursorPosition(3, 1);
+            lcd.Write("Derp!");            
+        }
+    }
+}
---
+++ PiCamera/PiCamera/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using MMALSharp;
+using MMALSharp.Common;
+using MMALSharp.Handlers;
+
+namespace PiCamera
+{
+    class Program
+    {
+
+        static void TakePicture()
+        {
+            MMALCamera cam = MMALCamera.Instance;
+            MMALCameraConfig.ShutterSpeed = 2000000;
+
+            using (var imgCaptureHandler = new ImageStreamCaptureHandler("/home/pi/images/", "jpg"))
+            {
+                Console.WriteLine("Taking Picture.");
+                cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
+            }         
+
+            Console.WriteLine("Cleanup Starting.");
+            cam.Cleanup();
+            Console.WriteLine("Done with Cleanup.");
+        }
+
+
+        static void Main()
+        {
+            TakePicture();
+            
+        }
+    }
+}
